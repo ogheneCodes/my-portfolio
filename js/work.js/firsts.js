ce1e3a7341abb7b3a7b9443d9faf51e27c6106a9
EBOBO MAXWELL OGHENEMAGA <oghenemaga.ebobo@unn.edu.ng>
2021-11-08 13:20:07 +0100

    added a work.js folder with a first.js file to be deleted later

---
+++ js/work.js/firsts.js
@@ -0,0 +1,8 @@
+let person = {
+    userName = 'Maxbobo',
+    condition = 'work in progress',
+    gender = 'male',
+}
+console.log(person.name);
+console.log(person.condition);
+console.log(person.gender);
